76d80379968d5e8724c06ef82eec5ee86b645f57
joshtune <joshua.tune@gmail.com>
2020-11-23 21:59:16 -0700

    add routing

---
+++ apps/me/src/app/app-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from "./app.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full',
+  // },
+  // {
+  //   path: 'login',
+  //   component: LoginPageComponent,
+  // },
+  // {
+  //   path: '',
+  //   component: MainLayoutComponent,
+  //   children: [
+  //     {
+  //       path: 'home',
+  //       loadChildren: () =>
+  //         import('./features/home/home.module').then((m) => m.HomeModule),
+  //     },
+  //     {
+  //       path: 'sacrament',
+  //       loadChildren: () =>
+  //         import('./features/sacrament/sacrament.module').then(
+  //           (m) => m.SacramentModule
+  //         ),
+  //     },
+  //     {
+  //       path: 'cheat-sheet',
+  //       canActivate: [AuthGuard],
+  //       loadChildren: () =>
+  //         import('./features/cheat-sheet/cheat-sheet.module').then(
+  //           (m) => m.CheatSheetModule
+  //         ),
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '**',
+  //   component: PageNotFoundPageComponent,
+  // },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- apps/me/src/app/app.component.ts
+++ apps/me/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'joshtune-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: ` <router-outlet></router-outlet> `,
 })
-export class AppComponent {
-  title = 'me';
-}
+export class AppComponent {}
