aea95a2ef0dc4da64c7462cb00ed71238f90cba0
joshtune <joshua.tune@gmail.com>
2020-11-23 22:01:39 -0700

    add scully

--- apps/me/src/app/app.module.ts
+++ apps/me/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ scully.me.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./apps/me/src",
+  projectName: "me",
+  outDir: './dist/static',
+  routes: {
+  }
+};
